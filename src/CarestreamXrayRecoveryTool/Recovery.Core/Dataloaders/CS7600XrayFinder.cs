1123e51d43a6da70aadff52260c74bafb2703083
JamesDavidWright <128067914+JamesDavidWright@users.noreply.github.com>
2023-11-02 18:48:27 +0000

    No patient found error added
    Added no found patients error to CS7600XrayFinder class. Removed logic for this from test. Also improved comments for CS7600XrayFinder class

--- src/CarestreamXrayRecoveryTool/Recovery.Core/Dataloaders/CS7600XrayFinder.cs
+++ src/CarestreamXrayRecoveryTool/Recovery.Core/Dataloaders/CS7600XrayFinder.cs
@@ -8,24 +8,50 @@ using System.Xml.Linq;
 
 namespace Recovery.Core.Dataloaders
 {
+    /// <summary>
+    /// The CS7600XrayFinder class is responsible for searching and retrieving lost X-rays from 
+    /// specific directories on the system. It is designed to work on both 32-bit and 64-bit 
+    /// installations of the software.
+    /// </summary>
     public class CS7600XrayFinder
     {
+        // Constants to define the directories where X-ray studies might be located. 
+        // These paths are dependent on the architecture of the system (32 or 64 bit).
         private const string StudiesPath32 = @"C:\Program Files (x86)\CSH\CS7600\images\studies";
         private const string StudiesPath64 = @"C:\Program Files\CSH\CS7600\images\studies";
 
+        /// <summary>
+        /// This method initiates the search for lost X-rays and returns the findings in 
+        /// a structured format.
+        /// </summary>
+        /// <returns>A FinderResult object containing the search result and any lost X-rays found.</returns>
         public FinderResult FindXrays()
         {
             var result = new FinderResult();
+
+            // First, determine the correct path to the studies directory based on system architecture.
             var studiesDirectory = GetStudiesDirectoryPath();
 
+            // If we couldn't determine the path, something's wrong.
             if (string.IsNullOrEmpty(studiesDirectory))
             {
                 result.Success = false;
-                result.ErrorMessage = "Failed to locate the default directory.";
+                result.ErrorMessage = "Failed to locate the studies directory.";
                 return result;
             }
 
+            // Fetch any lost X-rays from the studies directory.
             result.LostXrays = FindFromStudiesDirectory(studiesDirectory);
+
+            // If no X-rays are found, we want to provide a clear error message.
+            if (result.LostXrays.Count == 0)
+            {
+                result.Success = false;
+                result.ErrorMessage = "No lost X-rays found in the studies directory.";
+                return result;
+            }
+
+            // If no errors encountered during the process, mark the search as successful.
             if (string.IsNullOrEmpty(result.ErrorMessage))
             {
                 result.Success = true;
@@ -33,8 +59,14 @@ namespace Recovery.Core.Dataloaders
             return result;
         }
 
+        /// <summary>
+        /// Determines the correct studies directory based on whether the system 
+        /// has a 32-bit or 64-bit installation of the software.
+        /// </summary>
+        /// <returns>The path to the studies directory or null if none exists.</returns>
         private string? GetStudiesDirectoryPath()
         {
+            // Check for existence of directories and return the appropriate path.
             if (Directory.Exists(StudiesPath32))
             {
                 return StudiesPath32;
@@ -49,13 +81,21 @@ namespace Recovery.Core.Dataloaders
             }
         }
 
+        /// <summary>
+        /// Searches the provided studies directory for any lost X-rays.
+        /// </summary>
+        /// <param name="studiesFolderPath">The path to the studies directory.</param>
+        /// <returns>A list of lost X-rays found within the studies directory.</returns>
         private List<LostXray> FindFromStudiesDirectory(string studiesFolderPath)
         {
             List<LostXray> allLostXrays = new List<LostXray>();
+
             try
             {
+                // Retrieve all patient directories within the studies directory.
                 var patientDirectories = Directory.GetDirectories(studiesFolderPath);
 
+                // Iterate through each patient directory to search for X-rays.
                 foreach (var patientFolderPath in patientDirectories)
                 {
                     var lostXraysForPatient = FindFromPatientFolder(patientFolderPath);
@@ -64,23 +104,33 @@ namespace Recovery.Core.Dataloaders
             }
             catch (Exception ex)
             {
+                // Handle any errors encountered during the process.
                 return new FinderResult { ErrorMessage = $"Error encountered while processing the studies directory at {studiesFolderPath}: {ex.Message}" }.LostXrays;
             }
             return allLostXrays;
         }
 
+        /// <summary>
+        /// Searches a patient's folder for any lost X-rays.
+        /// </summary>
+        /// <param name="patientFolderPath">The path to a patient's directory.</param>
+        /// <returns>A list of lost X-rays found within the patient's directory.</returns>
         private List<LostXray> FindFromPatientFolder(string patientFolderPath)
         {
             List<LostXray> lostXrays = new List<LostXray>();
+
             try
             {
+                // Locate the XML file containing patient details within the directory.
                 string inputDataPath = Path.Combine(patientFolderPath, "InputData.xml");
 
+                // If the XML file doesn't exist, we can't proceed further for this patient.
                 if (!File.Exists(inputDataPath))
                 {
                     return lostXrays;
                 }
 
+                // Load the XML content to fetch patient details.
                 XElement inputDataXml = XElement.Load(inputDataPath);
 
                 string? patientFirstName = inputDataXml.Element("Patient_FirstName")?.Value.Trim();
@@ -91,16 +141,20 @@ namespace Recovery.Core.Dataloaders
                     return lostXrays;
                 }
 
+                // Search for any subdirectories within the patient's directory, 
+                // each potentially containing an X-ray.
                 var xraySubDirectories = Directory.GetDirectories(patientFolderPath);
 
                 foreach (var xraySubDir in xraySubDirectories)
                 {
+                    // Look for any lost X-ray file and its associated preview image.
                     string? lostXrayPath = Directory.GetFiles(xraySubDir, "U*.dcm").FirstOrDefault();
                     DateTime? dateTaken = lostXrayPath != null ? File.GetLastWriteTime(lostXrayPath) : (DateTime?)null;
                     string? previewImagePath = Directory.GetFiles(xraySubDir, "*.jpg").FirstOrDefault();
 
                     if (lostXrayPath != null && previewImagePath != null && dateTaken.HasValue)
                     {
+                        // Create an object representation of the found X-ray.
                         var lostXray = new LostXray(patientFirstName, patientLastName, dateTaken.Value, previewImagePath, lostXrayPath);
                         lostXrays.Add(lostXray);
                     }
@@ -108,6 +162,7 @@ namespace Recovery.Core.Dataloaders
             }
             catch (Exception ex)
             {
+                // Handle any errors encountered during the process.
                 return new FinderResult { ErrorMessage = $"Error encountered while processing the patient directory at {patientFolderPath}: {ex.Message}" }.LostXrays;
             }
             return lostXrays;
--- src/CarestreamXrayRecoveryTool/Recovery.Tests/Program.cs
+++ src/CarestreamXrayRecoveryTool/Recovery.Tests/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Recovery.Core.Models;
-using Recovery.Core.Dataloaders;   // Namespace for CS7600XrayFinder
+using Recovery.Core.Dataloaders;
 using Recovery.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -10,74 +10,116 @@ namespace CS7600XrayRecover.ConsoleTest
     {
         static void Main(string[] args)
         {
-            // Creating an instance of the new CS7600XrayFinder
+            // Provide the initial prompt to the user for searching X-rays
+            Console.WriteLine("Do you want to search for lost X-rays? (yes/no)");
+            string response = Console.ReadLine().ToLower();
+
+            // If the user's response isn't affirmative, exit the program
+            if (response != "yes")
+            {
+                Console.WriteLine("Exiting the program.");
+                EndProgram();
+                return;
+            }
+
+            // Creating an instance of the CS7600XrayFinder to search for lost X-rays
             CS7600XrayFinder xrayFinder = new CS7600XrayFinder();
 
             // Fetching the lost X-ray data for testing
             FinderResult finderResult = xrayFinder.FindXrays();
 
-            // Check if any lost X-rays were found
-            if (!finderResult.Success || finderResult.LostXrays.Count == 0)
+            // If no X-rays found or an error occurred, display the error message and exit
+            if (!finderResult.Success)
             {
-                Console.WriteLine(finderResult.ErrorMessage ?? "No lost X-rays found.");
+                Console.WriteLine(finderResult.ErrorMessage);
+                EndProgram();
+                return;
             }
-            else
+
+            // Display the lost X-rays found to the user
+            Console.WriteLine("Lost X-rays found:");
+            DisplayLostXrays(finderResult.LostXrays);
+
+            // Prompt the user to select a specific X-ray for recovery
+            LostXray selectedXray = GetUserSelection(finderResult.LostXrays);
+
+            // Initiate the recovery process for the selected X-ray
+            RecoverSelectedXray(selectedXray);
+
+            // Wait for user input before closing the console application
+            EndProgram();
+        }
+
+        /// <summary>
+        /// Display the list of lost X-rays to the user.
+        /// </summary>
+        /// <param name="lostXrays">List of lost X-rays</param>
+        private static void DisplayLostXrays(List<LostXray> lostXrays)
+        {
+            foreach (var xray in lostXrays)
             {
-                Console.WriteLine("Lost X-rays found:");
-                foreach (var xray in finderResult.LostXrays)
+                Console.WriteLine($"- Patient: {xray.PatientFirstName} {xray.PatientLastName}, Date Taken: {xray.DateTaken:yyyy-MM-dd}, Preview Image Path: {xray.PreviewImagePath}, Lost X-ray Path: {xray.LostXrayPath}");
+            }
+        }
+
+        /// <summary>
+        /// Prompt the user to select a specific X-ray for recovery.
+        /// </summary>
+        /// <param name="lostXrays">List of lost X-rays</param>
+        /// <returns>The selected X-ray</returns>
+        private static LostXray GetUserSelection(List<LostXray> lostXrays)
+        {
+            Dictionary<int, LostXray> patientChoices = new Dictionary<int, LostXray>();
+
+            Console.WriteLine("\nPlease select a patient by number for X-ray recovery:");
+
+            int choiceNumber = 1;
+            foreach (var xray in lostXrays)
+            {
+                Console.WriteLine($"{choiceNumber}. Patient: {xray.PatientFirstName} {xray.PatientLastName}, Date Taken: {xray.DateTaken:yyyy-MM-dd}");
+                patientChoices[choiceNumber] = xray;
+                choiceNumber++;
+            }
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int selectedNumber) && patientChoices.ContainsKey(selectedNumber))
                 {
-                    Console.WriteLine($"- Patient: {xray.PatientFirstName} {xray.PatientLastName}, Date Taken: {xray.DateTaken:yyyy-MM-dd}, Preview Image Path: {xray.PreviewImagePath}, Lost X-ray Path: {xray.LostXrayPath}");
-                }
-
-                // Creating a dictionary to map choice numbers to X-ray entries for user selection
-                Dictionary<int, LostXray> patientChoices = new Dictionary<int, LostXray>();
-
-                // Display a list of patients with lost X-rays and ask the user to select one by number
-                Console.WriteLine("\nPlease select a patient by number for X-ray recovery:");
-
-                int choiceNumber = 1;
-                foreach (var xray in finderResult.LostXrays)
-                {
-                    Console.WriteLine($"{choiceNumber}. Patient: {xray.PatientFirstName} {xray.PatientLastName}, Date Taken: {xray.DateTaken:yyyy-MM-dd}");
-                    patientChoices[choiceNumber] = xray;  // Map the choice number to the X-ray entry
-                    choiceNumber++;
-                }
-
-                // Loop to ensure user makes a valid selection
-                int selectedNumber;
-                while (true)
-                {
-                    // Try to parse user input as an integer and check if it matches one of the choices
-                    if (int.TryParse(Console.ReadLine(), out selectedNumber) && patientChoices.ContainsKey(selectedNumber))
-                    {
-                        break;  // Exit the loop once a valid choice is made
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid choice. Please select a valid number from the list above.");
-                    }
-                }
-
-                // Fetch the selected X-ray using the user's choice
-                LostXray selectedXray = patientChoices[selectedNumber];
-
-                // Initiate the RecoveryService
-                RecoveryService recoveryService = new RecoveryService();
-
-                // Use the RecoveryService to recover the selected X-ray
-                bool isRecovered = recoveryService.RecoverLostXray(selectedXray);
-
-                if (isRecovered)
-                {
-                    Console.WriteLine($"Successfully recovered X-ray for: {selectedXray.PatientFirstName} {selectedXray.PatientLastName}, Date Taken: {selectedXray.DateTaken:yyyy-MM-dd}");
+                    return patientChoices[selectedNumber];
                 }
                 else
                 {
-                    Console.WriteLine($"Failed to recover X-ray for: {selectedXray.PatientFirstName} {selectedXray.PatientLastName}, Date Taken: {selectedXray.DateTaken:yyyy-MM-dd}");
+                    Console.WriteLine("Invalid choice. Please select a valid number from the list above.");
                 }
             }
+        }
 
-            // Wait for user input before closing the console application
+        /// <summary>
+        /// Initiates the recovery process for the selected X-ray.
+        /// </summary>
+        /// <param name="selectedXray">The selected X-ray for recovery</param>
+        private static void RecoverSelectedXray(LostXray selectedXray)
+        {
+            // Using the RecoveryService to handle the recovery process
+            RecoveryService recoveryService = new RecoveryService();
+            bool isRecovered = recoveryService.RecoverLostXray(selectedXray);
+
+            // Notify the user about the success or failure of the recovery process
+            if (isRecovered)
+            {
+                Console.WriteLine($"Successfully recovered X-ray for: {selectedXray.PatientFirstName} {selectedXray.PatientLastName}, Date Taken: {selectedXray.DateTaken:yyyy-MM-dd}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to recover X-ray for: {selectedXray.PatientFirstName} {selectedXray.PatientLastName}, Date Taken: {selectedXray.DateTaken:yyyy-MM-dd}");
+            }
+        }
+
+        /// <summary>
+        /// A utility method to wait for user input before closing the console application.
+        /// </summary>
+        private static void EndProgram()
+        {
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
